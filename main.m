e1274e0fd454ffca9bffc0544c3b2e44ce617f32
Jakub Suder <jakub.suder@gmail.com>
2010-02-01 23:21:20 +0100

    initial commit - empty MacRuby application

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MacBlip
+//
+//  Created by Jakub Suder on 1.2.10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
