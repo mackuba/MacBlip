c9921bc59b67d10735f2742f85d265efb73b35de
Jakub Suder <jakub.suder@gmail.com>
2010-02-19 19:25:07 +0100

    prettier date display in message cells

--- Controllers/MessageCellController.rb
+++ Controllers/MessageCellController.rb
@@ -7,6 +7,17 @@
 
 class MessageCellController < SDListViewItem
 
+  attr_accessor :dateLabel
+
+  def self.dateFormatter
+    @dateFormatter ||= HumanReadableDateFormatter.new
+  end
+
+  def loadView
+    super
+    dateLabel.formatter = self.class.dateFormatter
+  end
+
   def heightForGivenWidth(width)
     self.view.frame.size.height
   end
---
+++ Models/HumanReadableDateFormatter.rb
@@ -0,0 +1,35 @@
+# -------------------------------------------------------
+# HumanReadableDateFormatter.rb
+#
+# Copyright (c) 2010 Jakub Suder <jakub.suder@gmail.com>
+# Licensed under GPL v3 license
+# -------------------------------------------------------
+
+class HumanReadableDateFormatter < NSFormatter
+
+  def init
+    @dateOnlyFormatter = NSDateFormatter.new
+    @dateOnlyFormatter.dateStyle = NSDateFormatterShortStyle
+    @dateOnlyFormatter.timeStyle = NSDateFormatterNoStyle
+
+    @timeOnlyFormatter = NSDateFormatter.new
+    @timeOnlyFormatter.dateStyle = NSDateFormatterNoStyle
+    @timeOnlyFormatter.timeStyle = NSDateFormatterMediumStyle
+
+    @timeAndDayFormatter = NSDateFormatter.new
+    @timeAndDayFormatter.dateFormat = "EEEE, HH:mm:ss"
+
+    self
+  end
+
+  def stringForObjectValue(date)
+    thatDate = @dateOnlyFormatter.stringFromDate(date)
+    today = @dateOnlyFormatter.stringFromDate(NSDate.date)
+    if thatDate == today
+      @timeOnlyFormatter.stringFromDate(date)
+    else
+      @timeAndDayFormatter.stringFromDate(date)
+    end
+  end
+
+end
