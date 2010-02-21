077d885f296dde819dc78d9ecd06a2fc51db8b4d
Jakub Suder <jakub.suder@gmail.com>
2010-02-21 16:39:25 +0100

    added support for private/directed messages in OBMessage

--- ObjectiveBlip/Classes/OBMessage.h
+++ ObjectiveBlip/Classes/OBMessage.h
@@ -10,21 +10,31 @@
 
 @class OBUser;
 
+typedef enum { OBStatusMessage, OBDirectedMessage, OBPrivateMessage } OBMessageType;
+
 @interface OBMessage : OBModel {
   // interesting stuff
   NSString *body;
   NSDate *date;
   OBUser *user;
+  OBUser *recipient;
+  OBMessageType messageType;
 
   // less interesting stuff
+  NSString *type;
   NSString *userPath;
+  NSString *recipientPath;
   NSString *createdAt;
 }
 
 @property (nonatomic, copy) NSString *body;
+@property (nonatomic, copy) NSString *type;
 @property (nonatomic, copy) NSString *userPath;
+@property (nonatomic, copy) NSString *recipientPath;
 @property (nonatomic, copy) NSDate *date;
 @property (nonatomic, copy) NSString *createdAt;
 @property (nonatomic, retain) OBUser *user;
+@property (nonatomic, retain) OBUser *recipient;
+@property (nonatomic) OBMessageType messageType;
 
 @end
--- ObjectiveBlip/Classes/OBMessage.m
+++ ObjectiveBlip/Classes/OBMessage.m
@@ -11,11 +11,11 @@
 
 @implementation OBMessage
 
-@synthesize userPath, body, date, user, createdAt;
-OnDeallocRelease(userPath, body, date, user, createdAt);
+@synthesize userPath, recipientPath, body, date, user, recipient, createdAt, messageType, type;
+OnDeallocRelease(userPath, recipientPath, body, date, user, recipient, createdAt, type);
 
 - (id) init {
-  return [super initWithProperties: OBArray(@"body", @"userPath", @"createdAt")];
+  return [super initWithProperties: OBArray(@"body", @"userPath", @"createdAt", @"recipientPath", @"type")];
 }
 
 - (void) setUserPath: (NSString *) path {
@@ -24,6 +24,26 @@ OnDeallocRelease(userPath, body, date, user, createdAt);
   userPath = [path copy];
   NSString *login = [[path componentsSeparatedByString: @"/"] lastObject];
   user = [[OBUser findOrCreateByLogin: login] retain];
+}
+
+- (void) setRecipientPath: (NSString *) path {
+  [recipientPath release];
+  [recipient release];
+  recipientPath = [path copy];
+  NSString *login = [[path componentsSeparatedByString: @"/"] lastObject];
+  recipient = [[OBUser findOrCreateByLogin: login] retain];
+}
+
+- (void) setType: (NSString *) typeName {
+  [type release];
+  type = [typeName copy];
+  if ([type isEqual: @"PrivateMessage"]) {
+    messageType = OBPrivateMessage;
+  } else if ([type isEqual: @"DirectedMessage"]) {
+    messageType = OBDirectedMessage;
+  } else {
+    messageType = OBStatusMessage;
+  }
 }
 
 + (NSDateFormatter *) timeZoneLessDateFormatter {
