8752e4da0b2d9df0c30db6c8cbd8be04d1bd7649
Jakub Suder <jakub.suder@gmail.com>
2010-02-22 23:13:20 +0100

    added bridges for ASIHTTPRequest classes

--- ObjectiveBlip/generate_bridges.sh
+++ ObjectiveBlip/generate_bridges.sh
@@ -3,7 +3,7 @@ DIR=`dirname $0`
 if [ -n "$1" ]; then
   FILES="$1"
 else
-  FILES="$DIR/Classes/*.h $DIR/*.h"
+  FILES="$DIR/Classes/*.h $DIR/*.h $DIR/Lib/ASIHTTPRequest/*.h"
 fi
 INCLUDES="-I$DIR -I$DIR/Classes -I$DIR/Lib/ASIHTTPRequest"
 
@@ -14,3 +14,5 @@ do
   gen_bridge_metadata -c "$INCLUDES" $file > $DIR/Bridges/`basename $file .h`.bridgesupport
   echo "Generated bridge for" `basename $file`
 done
+
+rm $DIR/Bridges/ASIAuthenticationDialog.bridgesupport
