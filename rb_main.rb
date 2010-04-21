d912e27991e68c0d49d99cc76eb4eba8b4893007
Jakub Suder <jakub.suder@gmail.com>
2010-04-21 19:47:53 +0200

    fixed loading of bridge support files from compiled .rbo files

--- rb_main.rb
+++ rb_main.rb
@@ -11,11 +11,11 @@ framework 'Cocoa'
 # Loading all the Ruby project files
 main = File.basename(__FILE__, File.extname(__FILE__))
 dir_path = NSBundle.mainBundle.resourcePath.fileSystemRepresentation
-Dir.glob(File.join(dir_path, '*.{rb,rbo}')).map { |x| File.basename(x, File.extname(x)) }.uniq.each do |path|
-  require(path) unless path == main
-end
 Dir.glob(File.join(dir_path, '*.bridgesupport')).uniq.each do |path|
   load_bridge_support_file(path)
+end
+Dir.glob(File.join(dir_path, '*.{rb,rbo}')).map { |x| File.basename(x, File.extname(x)) }.uniq.each do |path|
+  require(path) unless path == main
 end
 
 # Starting the Cocoa main loop
