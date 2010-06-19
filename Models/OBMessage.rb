5b64300aa2f320bd67a5f2cab83c13d0f423ca0f
Jakub Suder <jakub.suder@gmail.com>
2010-06-19 23:06:18 +0200

    fixed DPI bug in message pictures
    - this may have caused crashes and/or memory usage spikes

--- Models/OBMessage.rb
+++ Models/OBMessage.rb
@@ -47,6 +47,8 @@ class OBMessage
     pictureData = pictures.first && pictures.first['data']
     if pictureData
       image = NSImage.alloc.initWithData(pictureData)
+      rep = image.representations.first
+      image.size = NSMakeSize(rep.pixelsWide, rep.pixelsHigh)  # force correct size, even if DPI is weird
       minSize = [image.size.width, image.size.height].min
       image.imageCroppedToFitSize(NSMakeSize(minSize, minSize))
     else
