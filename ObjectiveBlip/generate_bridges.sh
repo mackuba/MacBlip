3209ae82329d159eeb1d7d8e708f9b295902ee29
Jakub Suder <jakub.suder@gmail.com>
2011-01-03 23:53:01 +0100

    updated ObjectiveBlip's MacRuby bridges

--- ObjectiveBlip/generate_bridges.sh
+++ ObjectiveBlip/generate_bridges.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 DIR=`dirname $0`
+ROOTDIR=`cd $DIR/..; pwd`
+PSIDIR="$ROOTDIR/Lib/PsiToolkit"
 if [ -n "$1" ]; then
   FILES="$1"
 else
-  FILES="$DIR/Classes/*.h $DIR/*.h $DIR/Lib/ASIHTTPRequest/*.h"
+  FILES="$DIR/Classes/*.h $DIR/*.h"
 fi
-INCLUDES="-I$DIR -I$DIR/Classes -I$DIR/Lib/ASIHTTPRequest -I$DIR/Lib/PsiToolkit"
+INCLUDES="-I$DIR -I$DIR/Classes -I$ROOTDIR/Lib/ASIHTTPRequest -I$PSIDIR/Models -I$PSIDIR/Network -I$PSIDIR/Security"
+INCLUDES="$INCLUDES -include PSAccount.h -include PSConnector.h"
 
 mkdir -p $DIR/Bridges
 
@@ -14,5 +17,3 @@ do
   gen_bridge_metadata -c "$INCLUDES" $file > $DIR/Bridges/`basename $file .h`.bridgesupport
   echo "Generated bridge for" `basename $file`
 done
-
-rm $DIR/Bridges/ASIAuthenticationDialog.bridgesupport
